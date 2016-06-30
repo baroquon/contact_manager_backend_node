aaadbe509ed0aaece3eba762cd23263ce095292d
Brandon Blaylock <brandon.blaylock@gmail.com>
2016-06-29 20:36:06 -0500

    cleaned this up and replaced the db with fixtures

---
+++ addresses_list.js
@@ -0,0 +1,63 @@
+module.exports = [
+  {
+    id: 1,
+    type: 'addresses',
+    attributes: {
+      street1: "1988 GHWB St",
+      street2: "",
+      city: "Boston",
+      state: "MA",
+      zip: "02116"
+    }
+  }, {
+    id: 2,
+    type: 'addresses',
+    attributes: {
+      street1: "1992 Clinton Ave",
+      street2: "",
+      city: "Little Rock",
+      state: "AR",
+      zip: "72207"
+    }
+  }, {
+    id: 3,
+    type: 'addresses',
+    attributes: {
+      street1: "1996 William St",
+      street2: "",
+      city: "Little Rock",
+      state: "AR",
+      zip: "72211"
+    }
+  }, {
+    id: 4,
+    type: 'addresses',
+    attributes: {
+      street1: "2000 Bush Pkwy", street2: "", city: "Austin", state: "TX", zip: "78703"
+    }
+  }, {
+    id: 5,
+    type: 'addresses',
+    attributes: {
+      street1: "2004 George Dr", street2: "Apt B", city: "Austin", state: "TX", zip: "78717"
+    }
+  }, {
+    id: 6,
+    type: 'addresses',
+    attributes: {
+      street1: "2008 Obama Ave", street2: "", city: "Springfield", state: "IL", zip: "62705"
+    }
+  }, {
+    id: 7,
+    type: 'addresses',
+    attributes: {
+      street1: "2012 Barack St", street2: "", city: "Springfield", state: "IL", zip: "62723"
+    }
+  }, {
+    id: 8,
+    type: 'addresses',
+    attributes: {
+      street1: "1984 Othness St", street2: "", city: "Birmingham", state: "AL", zip: "35209"
+    }
+  }
+]
